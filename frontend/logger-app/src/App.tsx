af1733484ce8789d455ca8ce68a02b7ec4ae082b
sambasile10 <sbasile2019@gmail.com>
2021-12-22 16:50:54 -0700

    Initialize frontend react app

---
+++ frontend/logger-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+class App extends React.Component<{}, {}> {
+
+
+    render() {
+        return (
+          <Container fluid>
+            
+          </Container>
+        );
+    }
+}
+
+export default App;
